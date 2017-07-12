64c05ea25d13281e0f6bb0cc4d2ae15459edd180
SergioStark <sgarcia@litoprocess.com>
2017-07-11 20:25:27 -0500

    Estructura base
    Estructura base de php y html

--- Mini-Framework/index.php
+++ Mini-Framework/index.php
@@ -3,8 +3,34 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Mini Framework PHP</title>
+	<style>
+		header{
+			background: #e3e3e3;
+			padding: 2em;
+			text-align: center;					
+		}
+		
+	</style>
 </head>
 <body>
-	<?php echo "Hola Mundo"; ?>
+	<header>
+
+		<?php 
+			$saludo = "Hola mundo";
+		?>
+
+		<h1>
+			<?php 
+			#'Este es un saludo' no importa el valor de saludo
+			echo "Este es un $saludo";
+
+			#Concatenar variables
+			echo 'Este es otro saludo' . $saludo;
+
+			echo "Imprimiendo saludo: {$saludo}"
+			?>			
+		</h1>
+
+	</header>
 </body>
 </html>
