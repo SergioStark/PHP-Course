b93eb4b63bf99fe664ad4b3b5962d323a065fbd4
SergioStark <sgarcia@litoprocess.com>
2017-07-12 20:23:37 -0500

    Arreglos

--- Mini-Framework/index.php
+++ Mini-Framework/index.php
@@ -1,10 +1,35 @@
 <?php 
+	//ARREGLOS
+	$nombres = array(
+		'Alejandro',
+		'Luis',
+		'David'
+		);
 
-			//Variable super global GET
-			//http://mini-framework.com/?saludo=Hola&nombre=Sergio&edad=%3Ch1%3E18%3C/h1%3E
-			$saludo = $_GET['saludo'];
-			$nombre = $_GET['nombre'];
-			$edad = $_GET['edad'];
+	$animales = [
+		'Perro',
+		'Gato',
+		'Leon'
+		];
+
+	//Traer un elemento en especifico
+	$dato = $animales[1];		
+
+	//Exite un elemento?
+	$respuesta = in_array('Sergio', $nombres);
+	if ($respuesta == 1) {
+		$respuesta2 = "Si";
+	} else {
+		//if ($respuesta === 0) {
+			$respuesta2 = "No";
+		//}
+	}
+
+	//Eliminar un elemento
+	unset($nombres[1]);
+
+
+//ARREGLOS ASOCIATIVOS
 
 
 require 'index.view.php';
--- Mini-Framework/index.view.php
+++ Mini-Framework/index.view.php
@@ -8,26 +8,46 @@
 			background: #e3e3e3;
 			padding: 2em;
 			text-align: center;					
+		}	
+
+		h2 {
+			color: green;
+			padding-left: 2em;
+			padding-top: 2em;
 		}
-		
+
+		li {
+			color: blue;
+		}	
 	</style>
 </head>
 <body>
 	<header>
-
-		<h1>
-			<?php 
-				echo $saludo . ' ' . $nombre;
-			?>						
-		</h1>
-		<br>
-		<?php echo $edad; ?>
-		<br>
-		
-		<!-- Short tag -->
-		<!--<?php //echo $variable ?> Es igual que escribir: <? //= $variable ?> -->
-		<?= $edad; ?>
-
+		<h1>Arreglos</h1>
 	</header>
+
+	<main>
+		<h2>Nombres:</h2>
+		<ul>
+		<?php foreach ($nombres as $nombre) { ?>
+			<li><?= $nombre ?></li>
+		<?php } ?>
+		</ul>	
+
+		<h2>Animales:</h2>
+		<ol>
+		<?php foreach ($animales as $animal) : ?>
+			<li><?= $animal ?></li>
+		<?php endforeach ?>		
+		</ol>
+
+		<p><?= $dato; ?></p>
+		<p>¿Existe Sergio? <?php echo $respuesta2; ?></p>
+
+	</main>
+
+	<footer>
+		
+	</footer>
 </body>
 </html>
